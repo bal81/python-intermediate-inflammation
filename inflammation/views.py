5d502561240909cf710f64104c8a7265d94df8d4
balder <b.villagomez@qub.ac.uk>
2024-11-26 15:30:14 +0000

    my first commit

--- inflammation/views.py
+++ inflammation/views.py
@@ -2,7 +2,7 @@
 
 from matplotlib import pyplot as plt
 import numpy as np
-
+import pandas as pd
 
 def visualize(data_dict):
     """Display plots of basic statistical properties of the inflammation data.
@@ -10,16 +10,19 @@ def visualize(data_dict):
     :param data_dict: Dictionary of name -> data to plot
     """
     # TODO(lesson-design) Extend to allow saving figure to file
-
+    
     num_plots = len(data_dict)
+
     fig = plt.figure(figsize=((3 * num_plots) + 1, 3.0))
 
     for i, (name, data) in enumerate(data_dict.items()):
+        df=pd.read_csv(data)
+        print(df.describe())
         axes = fig.add_subplot(1, num_plots, i + 1)
 
         axes.set_ylabel(name)
         axes.plot(data)
 
     fig.tight_layout()
-
+    plt.savefig('fig1.png')
     plt.show()
